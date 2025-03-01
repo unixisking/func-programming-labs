aaff80a48862289fca0f7af99ce86e55dc37f48b
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2025-03-01 13:00:51 +0100

    feat: add parser

--- src/main/scala/tp05/Parser.scala
+++ src/main/scala/tp05/Parser.scala
@@ -14,7 +14,14 @@ import scala.util.parsing.combinator.RegexParsers
  *    |  iszero t
  */
 class Parser extends RegexParsers {
-  def term : Parser[Term] = parenTerm | eof
+  def term : Parser[Term] = parenTerm | cond | truthy | falsy | zero | succ | pred | iszero | eof
   def parenTerm = ("("~>term<~")")
+  def truthy = "true" ^^ { _ => True }
+  def falsy = "false" ^^ { _ => False }
+  def zero = "0" ^^ { _ => Zero}
+  def succ = "succ" ~> term ^^ { term => Succ(term)}
+  def pred = "pred" ~> term ^^ { term => Pred(term)}
+  def iszero = "iszero" ~> term ^^ {term => IsZero(term)}
+  def cond = "if" ~ term ~ "then"  ~ term ~ "else" ~ term ^^ { case "if" ~ c ~ "then"  ~ first ~ "else" ~ second => Cond(c, first, second) }
   def eof = """\z""".r ^^ { _ => EOF }
 }
--- src/main/scala/tp05/Types.scala
+++ src/main/scala/tp05/Types.scala
@@ -6,7 +6,7 @@ case object EOF extends Term
 case object True extends Term
 case object False extends Term
 case object Zero extends Term
-case class If(condition: Term, truthy: Term, falsy: Term) extends Term
+case class Cond(condition: Term, truthy: Term, falsy: Term) extends Term
 case class Succ(t: Term) extends Term
 case class Pred(t: Term) extends Term
-case class isZero(t: Term) extends Term
+case class IsZero(t: Term) extends Term
