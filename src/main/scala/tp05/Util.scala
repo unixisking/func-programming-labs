0637f2a4df8a349bbf9f754ce496d8be3ff8b637
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2025-03-01 13:31:53 +0100

    feat: add utils functions for evaluator

--- src/main/scala/tp05/Util.scala
+++ src/main/scala/tp05/Util.scala
@@ -1,9 +1,33 @@
 package tp05
 
+/* 
+Values: 
+    True, False, Zero, Succ*(Zero)
+ Numerical Values: 
+    Zero, Succ*(Zero) 
+ */
 object Util {
   /** t est-il une valeur ? */
-  def isVal(t : Term) : Boolean = ???
+  def isVal(t : Term) : Boolean = t match
+    case EOF => false
+    case True => true
+    case False => true
+    case Zero => true
+    case Cond(condition, truthy, falsy) => false
+    case Succ(t) => isVal(t)
+    case Pred(t) => false
+    case IsZero(t) => false
+  
 
   /** t est-il une valeur numérique ? */
-  def isNumVal(t : Term) : Boolean = ???
+  def isNumVal(t : Term) : Boolean = t match
+    case EOF => false
+    case True => false
+    case False => false
+    case Zero => true
+    case Cond(condition, truthy, falsy) => false
+    case Succ(t) => isNumVal(t)
+    case Pred(t) => false
+    case IsZero(t) => false
+  
 }
