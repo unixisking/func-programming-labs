a139c11ff00a17dbf91fdfa7ecc76cfc16a2dc61
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2025-03-01 13:33:44 +0100

    fix: refactor evaluator util functions

--- src/main/scala/tp05/Util.scala
+++ src/main/scala/tp05/Util.scala
@@ -9,25 +9,16 @@ Values:
 object Util {
   /** t est-il une valeur ? */
   def isVal(t : Term) : Boolean = t match
-    case EOF => false
     case True => true
     case False => true
     case Zero => true
-    case Cond(condition, truthy, falsy) => false
     case Succ(t) => isVal(t)
-    case Pred(t) => false
-    case IsZero(t) => false
+    case _ => false
   
 
   /** t est-il une valeur numérique ? */
   def isNumVal(t : Term) : Boolean = t match
-    case EOF => false
-    case True => false
-    case False => false
     case Zero => true
-    case Cond(condition, truthy, falsy) => false
     case Succ(t) => isNumVal(t)
-    case Pred(t) => false
-    case IsZero(t) => false
-  
+    case _ => false
 }
